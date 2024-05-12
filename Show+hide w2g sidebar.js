e4c6631b8695dd3cf6364f7f16b61b9cd0246aa5
Foxscotch <foxscotch@gmail.com>
2024-05-12 15:51:49 -0500

    Add more w2g ui enhancements

--- Show+hide w2g sidebar.js
+++ Show+hide w2g sidebar.js
@@ -1,12 +1,24 @@
 // ==UserScript==
-// @name        Show/hide w2g sidebar
+// @name        w2g ui enhancements
 // @namespace   w2g
 // @match       https://w2g.tv/en/room/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Foxscotch
-// @description Changes the help/feedback button on the right to a button that toggles the main sidebar
+// @description A few small enhancements to the w2g ui
 // ==/UserScript==
+
+// Fix the weirdness with the video player's max height and width
+
+const playerSearch = document.getElementsByClassName("w2g-player-search")[0];
+playerSearch.style.maxWidth = "100%";
+
+const videoContainer = document.getElementsByClassName(
+  "w2g-video-container"
+)[0];
+videoContainer.style.maxHeight = "calc(100vh - 100px)"; // just enough to be out of the way of the user list button thing
+
+// Changes the help/feedback button on the right to a button that toggles the main sidebar
 
 const toggleButton = document.getElementsByClassName("w2g-feedback")[0];
 toggleButton.innerHTML = "Toggle sidebar";
